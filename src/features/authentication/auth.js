7ec1134bd40233fb37210156e138b01dd98b9d2c
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2025-11-09 13:16:16 +0500

    Redux JS Toolkit configed and redux store added

---
+++ src/features/authentication/auth.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    loginStatus: false;
+    userData = null;
+}
+
+const authSlice = createSlice({
+    name: "authentication",
+    initialState,
+    reducers: {
+        login: (state, action) {
+            state.loginStatus = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) {
+            state.loginStatus = false;
+            state.userData = null;
+        }
+    }
+});
+
+export default authSlice.reducer;
+export const { login, logout } = authSlice.actions;
